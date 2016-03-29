b6a5d668432c2c52fc5e73b671ac023cb894eba7
Asım Arslan <arslanasim@gmail.com>
2016-03-29 16:08:17 +0300

    fix python script test

--- python-controller/tests/remote_controller_test.py
+++ python-controller/tests/remote_controller_test.py
@@ -59,28 +59,10 @@ class RemoteControllerTestCase(unittest.TestCase):
         cluster = self.controller.createCluster(None, None)
         member = self.controller.startMember(cluster.id)
         script = """
-def echo():
+def serialize():
     return instance_0.getSerializationService().toBytes(1.0)
-result=echo()
+result=serialize()
 """
         response = self.controller.executeOnController(cluster.id, script, Lang.PYTHON)
+        self.assertIsNotNone(response.result)
 
-
-        print(response)
-        # def test_multi_cluster(self):
-        #     cluster_1 = self.controller.createCluster(None, None)
-        #     member_1_1 = self.controller.startMember(cluster_1.id)
-        #     member_1_2 = self.controller.startMember(cluster_1.id)
-        #
-        #     cluster_2 = self.controller.createCluster(None, None)
-        #     member_2_1 = self.controller.startMember(cluster_2.id)
-        #     member_2_2 = self.controller.startMember(cluster_2.id)
-        #
-        #     self.assertTrue(member_2_1)
-        #     self.assertTrue(member_2_2)
-        #     self.assertTrue(member_1_1)
-        #     self.assertTrue(member_1_2)
-
-
-if __name__ == '__main__':
-    unittest.main()
