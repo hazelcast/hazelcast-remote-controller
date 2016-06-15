1921cfcd1e75ab667e7c9536666b4c22bbbd479e
Asım Arslan <arslanasim@gmail.com>
2016-06-15 15:51:33 +0300

    python lib setup fix

---
+++ python-controller/README.rst
@@ -0,0 +1,44 @@
+Hazelcast Remote Controller Python Library
+------------------------------------------
+
+Hazelcast python client for Remote Controller Project which is a cluster lifecycle manager 
+for native client tests. Server container and language clients provide easy management of 
+server side from clients
+
+* Create a cluster with a provided xml config
+* Start a Member in the configured cluster
+* Shutdown a member
+* Terminate a member
+* Remote script execution on the container
+
+Project uses Apache Thrift to provide multi language support.
+
+Installation
+------------
+
+You can install the Hazelcast python client via the following command::
+
+    $ pip install hazelcast-remote-controller
+
+or::
+
+    $ python setup.py install
+
+Mail Group
+----------
+
+Please join the mail group if you are interested in using or developing Hazelcast.
+
+`http://groups.google.com/group/hazelcast <http://groups.google.com/group/hazelcast>`_
+
+License
+~~~~~~~
+
+Hazelcast is available under the Apache 2 License. Please see the `Licensing appendix <http://docs.hazelcast.org/docs/latest/manual/html-single/hazelcast-documentation.html#license-questions>`_ for more information.
+
+Copyright
+~~~~~~~~~
+
+Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+
+Visit `www.hazelcast.com <http://www.hazelcast.com/>`_ for more info.
--- python-controller/setup.py
+++ python-controller/setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-# from codecs import open
-# from os import path
-# here = path.abspath(path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
         name='hazelcast-remote-controller',
         version='0.1',
         description='Hazelcast Remote Controller Python Library',
-        license='Apache License 2.0',
+        long_description=long_description,
+        url='https://github.com/hazelcast/hazelcast-remote-controller',
+        author='Hazelcast Inc. Developers',
+        author_email='hazelcast@googlegroups.com',
         classifiers=[
             #   3 - Alpha
             #   4 - Beta
@@ -21,10 +29,8 @@ setup(
             'Programming Language :: Python :: 2.7',
             'Topic :: Software Development :: Libraries :: Python Modules'
         ],
+        license='Apache 2.0',
         keywords='hazelcast,client,python,test',
-        # packages=find_packages(exclude=['examples', 'docs', 'tests']),
         packages=['hzrc'],
-        # namespace_packages=['hazelcast'],
-        # package_dir={'hazelcast': 'src'},
         install_requires=['thrift'],
 )
